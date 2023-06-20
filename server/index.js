052dc1b62da647928593cb8a86f38a8a5e2882a7
unknown <marrta.lm@gmail.com>
2023-06-20 15:06:38 +0300

    Initial

--- server/index.js
+++ server/index.js
@@ -3,6 +3,28 @@ const app = express()
 
 
 
+mongoose = require('mongoose')
+require('dotenv').config()
+var cors = require('cors')
+
+
+
+app.use(cors())
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+
+async function connecting(){
+    try {
+        await mongoose.connect(process.env.MONGO) 
+        console.log('Connected to the DBBBB')
+    } catch ( error ) {
+        console.log('ERROR: Seems like your DB is not running, please start it up !!!');
+    }
+    }
+    connecting();
+
+
+
 app.listen(4000,  ()=> {
     console.log('listening on port 4000!')
 })
