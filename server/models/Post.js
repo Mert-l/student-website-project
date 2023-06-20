4239cd485d3d85edcb3f4ac10ff53ccf295d31e9
unknown <marrta.lm@gmail.com>
2023-06-20 15:43:11 +0300

    schema v1

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
-    userId: { type: String, required: true, unique: true },
+ 
     title: { type: String, required: true },
     image: { type: Image, required: false, unique: false },
-    userId: { type: String, required: true, unique: true},
+    userId: { type: mongoose.Schema.Types.ObjectId, required: true, unique: true},
     price: { type: Number, required: false, unique: false, default: 0 },
     tags: { type: Array, required: false, unique: false },
     interested: [{user_id: {type: mongoose.Schema.Types.ObjectId,ref: 'users',}, _id: false,}],
--- server/models/User.js
+++ server/models/User.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  esername: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
   password: { required: true, unique: true },
-  userPosts : { type: Array, required: true, unique: false },
+  userPosts : [{post_id: {type: mongoose.Schema.Types.ObjectId, ref: 'posts',}}], // do same as down
   city: { type: String, required: true },
-  school: { type: String, required: true },
-  likedPost : [{post_id: {type: mongoose.Schema.Types.ObjectId, ref: 'posts',}, _id: false,}],,
+  likedPost : [{post_id: {type: mongoose.Schema.Types.ObjectId, ref: 'posts',}, _id: false,}],
+  degree : [{level: {type:String}}, {degree_name: {type:String}},  {sclool: {type:String}},]
 })
 
 
