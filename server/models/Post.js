dd75aef2de6c8ce224b6195bbd80aa0b89ea4fb3
unknown <marrta.lm@gmail.com>
2023-06-21 16:55:13 +0300

    Initial

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,17 +1,19 @@
 const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
- 
     title: { type: String, required: true },
     image: { type: String, required: false, unique: false },
-    userId: { type: String, required: true, unique: false},
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
     price: { type: Number, required: false, unique: false, default: 0 },
-    tags: { type: Array, required: false, unique: false },
-    interested: [{user_id: {type: mongoose.Schema.Types.ObjectId,ref: 'user',}, _id: false,}],
-    type :{type: String}
-    
-    
-    })
+    tags: [{ type: String, required: false, unique: false }],
+    interested: [
+      {
+        userId: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+        _id: false,
+      },
+    ],
+    type: { type: String },
+  });
 
 
 module.exports = mongoose.model("post", postSchema);
