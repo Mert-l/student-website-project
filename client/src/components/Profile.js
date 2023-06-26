232db12c635036558a6aecd891977d56853ca408
unknown <marrta.lm@gmail.com>
2023-06-26 18:03:02 +0300

    dfvdr

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import React, {useState, useEffect} from "react";
 
 const Profile = (props) => {
-  const [user_, setUser] = useState(null);
+  const [user_, setUser] = useState('');
 
   const fetchUser = async () => {
     try {
@@ -27,7 +27,40 @@ const Profile = (props) => {
 
   return (
 
-<h1> {user_.city} </h1>
+      <div className= 'profile_box' >   
+
+<h1> {!user_ ? <h2>no info yet</h2> : <div className='profile_container' >  
+
+<div className='first_half' > 
+    {/* <img src='./images/download.jpg'/ > */}
+    <h1>Image</h1>
+    <div className='bio_and_stuff'  >
+
+      <h3> {user_.username} </h3>
+      <h3> {user_.city} </h3>
+      <h3> {user_.description}</h3>
+
+
+
+    </div>
+
+
+ </div>      
+
+
+
+
+
+
+</div> } </h1>
+
+
+
+
+
+      </div>
+
+
   )
 };
 
