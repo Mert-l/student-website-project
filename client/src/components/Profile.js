09a638823d3b879aa9ca29d7983503733f78f88c
unknown <marrta.lm@gmail.com>
2023-06-27 16:23:09 +0300

    dfvdr

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -35,7 +35,7 @@ const Profile = (props) => {
 
       const fetchPosts = async () => {
         try {
-          const response = await axios.get('http://localhost:4000/post/getUserPosts', {userId: props.user._id})
+          const response = await axios.post('http://localhost:4000/post/getUserPosts', {userId: props.user._id})
           console.log( 'post response:' , response)
       setPosts(response.data)
         } catch (error) {
