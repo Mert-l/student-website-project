da0fa59987492245a3457b5474633438b9fc5ca7
unknown <marrta.lm@gmail.com>
2023-06-27 12:02:23 +0300

    dfvdr

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -6,6 +6,7 @@ import React, {useState, useEffect} from "react";
 const Profile = (props) => {
   const [ user_, setUser ] = useState(null)
   const [ message, setMessage ] = useState('')
+  const [dis, setDis] = useState(true)
 
   const fetchUser = async () => {
     try {
@@ -31,7 +32,7 @@ const Profile = (props) => {
 
 
       const handleSubmit = async (e) =>{
-        // e.preventDefault();
+       e.preventDefault();
         console.log('submits')
         try{
               if(user_){
@@ -58,12 +59,12 @@ const Profile = (props) => {
 
     {/* <img src='https://images.pexels.com/photos/4202203/pexels-photo-4202203.jpeg?auto=compress&cs=tinysrgb&w=600' /> */}
     <form className='bio_and_stuff' onSubmit={handleSubmit} onChange={handleChange} >
-
-    <input  placeholder='email'   name='email' value={props.user.email && props.user.email} />
-    <input placeholder='username' name='username' value={user_.username && user_.username}  />
-    <input placeholder='degree'  name='degree' value={user_.degree && user_.degree} />
-    <input placeholder='City'  name='city' value={user_.city && user_.city}  />
-    <input placeholder='Bio'  name='bio'  value={user_.bio && user_.bio} />
+    <button onClick={ () => setDis(!dis) }  ></button>
+    <input  placeholder='email'   name='email' value={props.user.email && props.user.email} disabled={dis}  />
+    <input placeholder='username' name='username' value={user_.username && user_.username} disabled={dis} />
+    <input placeholder='degree'  name='degree' value={user_.degree && user_.degree}  disabled={dis} />
+    <input placeholder='City'  name='city' value={user_.city && user_.city}  disabled={dis}  />
+    <input placeholder='Bio'  name='bio'  value={user_.bio && user_.bio} disabled={dis} />
     <button type="submit" className= 'box_item'  >Save changes</button>
     <h3> {message} </h3>
 
