df720d992eda05862f443112341f8772a1c780a2
unknown <marrta.lm@gmail.com>
2023-07-03 15:54:29 +0300

    sdcsfcsv

--- client/src/components/ViewProfile.js
+++ client/src/components/ViewProfile.js
@@ -3,18 +3,20 @@ import { NavLink } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import {useNavigate} from 'react-router-dom';
 import axios from 'axios';
+import {useLocation} from 'react-router-dom';
 
 const ViewProfile = (props) => {
   
-
+    const location = useLocation();
 const [user, setUser] = useState(null)
+console.log('user got with location:' , location.state.user )
 
     const fetchUser = async () => {
   
         try {
          
           const response = await axios.post("http://localhost:4000/user/getUser", {
-            _id: props.id
+            _id: location.state._id
           });
             console.log('does it even work')
           console.log("fetchet user i hope: ", response);
